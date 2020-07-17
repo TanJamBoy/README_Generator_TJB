912e3378945552af871a88ffde5aacc9874bf4b2
Tanner Boyer <tannerjboyer@gmail.com>
2020-07-16 17:22:53 -0700

    added questions to the 'questions' array, added a 'promptUser' function and called inquirer to ask the questions and write them to file

--- index.js
+++ index.js
@@ -1,23 +1,76 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const util = require("util");
+const Markdown = require("./utils/generateMarkdown");
 
 const writeFileAsync = util.promisify(fs.writeFile);
 
 
 // array of questions for user
 const questions = [
-    
+    "What is the title of your project?",
+    "What is the description of your project?",
+    "What are the installation requirements of your project?",
+    "How should people use your project?",
+    "Who are the collaborators on this project?",
+    "How should people test your project?",
+
 ];
 
+// prompt function
+function promptUser(){
+    inquirer
+        .prompt([
+            {
+                name: "title",
+                message: questions[0],
+                type: "input"
+            },
+            {
+                name: "desc",
+                message: questions[1],
+                type: "input"
+            },
+            {
+                name: "install",
+                message: questions[2],
+                type: "input"
+            },
+            {
+                name: "usage",
+                message: questions[3],
+                type: "input"
+            },
+            {
+                name: "collab",
+                message: questions[4],
+                type: "input"
+            },
+            {
+                name: "test",
+                message: questions[5],
+                type: "input"
+            },
+        ])
+        .then(e => {
+            let readmeContent = Markdown(e);
+            wrtieToFile("README_TEST.md", readmeContent);
+        })
+        .catch(err => {
+            if(err){console.log(err)};
+        });
+};
+
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function wrtieToFile(fileName, data) {
+    data = JSON.stringify(data);
+    writeFileAsync(fileName, data);
+};
 
 // function to initialize program
 function init() {
-
-}
+    promptUser();
+};
 
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,11 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+    ${data.desc}
+    ${data.install}
+    ${data.usage}
+    ${data.collab}
+    ${data.test}
 `;
 }
 
